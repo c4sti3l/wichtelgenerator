1269b4d81f821cf6e58829c5c0914b53f3fb151e
Christopher Hennenberg <chennenberg@gmx.de>
2024-12-03 00:21:56 +0100

    Deployment Pipeline todos

--- resources/app/views/pages/private/dashboard/Main.vue
+++ resources/app/views/pages/private/dashboard/Main.vue
@@ -3,8 +3,6 @@
     <div class="w-1/2 m-auto text-center pt-10">
       <h1 class="text-6xl mb-4 font-bold text-gray-600">Welcome</h1>
       <h4 class="text-gray-500 text-2xl">{{ new Date() }}</h4>
-
-      Test23xxxxxxaaaa
     </div>
     <footer class="py-5 text-center">
       <p class="text-gray-400">{{ trans('global.phrases.inspire') }}</p>
